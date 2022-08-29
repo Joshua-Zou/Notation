b268e75f9bac4418de448745d4116387ff910815
Joshua-Zou <77520157+Joshua-Zou@users.noreply.github.com>
2022-08-29 10:41:01 -0700

    Fix build errors

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import Navbar from "./components/navbar.js"
-import Footer from "./components/footer.js"
+import Footer from "./components/Footer.js"
 import 'semantic-ui-css/semantic.min.css'
 
 function MyApp({ Component, pageProps }) {
